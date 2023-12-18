fbae3a1bcd12868d7bcb863dbede31c354aa2d99
Zhanserik Nurym <220401@astanait.edu.kz>
2023-12-18 23:26:39 +0600

    Формы, Кастомизация шаблонов и форма, Регистрация и Авторизация

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Clothing-Store-Django
 
-Hello
--- users/views.py
+++ users/views.py
@@ -1,9 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from users.models import Users
+from users.forms import UserLoginForm, UserRegistrationForm
+from django.contrib import auth
+from django.urls import reverse
 
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse("index"))
+                pass
+    else:
+        form = UserLoginForm()
+
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
 
 
 def register(request):
-    return render(request, 'users/register.html')
+    if request.method == "POST":
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("users:login"))
+    else:
+        form = UserRegistrationForm()
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
